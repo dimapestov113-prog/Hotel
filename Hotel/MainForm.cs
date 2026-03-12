9749e1adcadf84775e3d5808cad195708a4e014b
dimapestov113@gmail.com <dimapestov113@gmail.com>
2026-03-12 10:14:49 +0400

    Добавьте файлы проекта.

---
+++ Hotel/MainForm.cs
@@ -0,0 +1,10 @@
+namespace Hotel
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Hotel/Models/Guest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Models
+{
+    public class Guest
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Middle_name { get; set; }
+        public string Phone { get; set; }
+        public string Passport{ get; set; }
+    }
+}
